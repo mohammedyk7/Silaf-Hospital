d4a28d66b8c58e88f863513bcd1e7fc9a8085879
mohammedyk7 <mohammedyk7@gmail.com>
2025-07-28 09:10:16 +0400

    adding all classes

--- Silaf Hospital/Models/user.cs
+++ Silaf Hospital/Models/user.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Silaf_Hospital.Models
 {
-    class user
+    public class User
     {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!; // "Admin", "Doctor", "Patient", etc.
+        public bool IsActive { get; set; } = true;
+
+        // Parameterless constructor (required by EF Core)
+        public User() { }
+
+        // Optional constructor for easier object creation
+        public User(string fullName, string email, string password, string role)
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+            Role = role;
+            IsActive = true;
+        }
     }
 }
